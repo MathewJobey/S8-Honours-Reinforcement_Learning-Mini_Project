83b0c677c9a41851f9750083cebfb4c60f29ffe6
MathewJobey <mathewjobey17@gmail.com>
2026-02-16 23:52:14 +0530

    changing geometry to resemble starship

--- rocket_env/settings.py
+++ rocket_env/settings.py
@@ -2,31 +2,30 @@
 
 # Screen / Rendering
 FPS = 60
-SCALE =20.0   #  affects how large the screen elements are
-VIEWPORT_W = 1000
-VIEWPORT_H = 700
+SCALE =5.0   #  affects how large the screen elements are
+VIEWPORT_W = 800
+VIEWPORT_H = 800
 
 # Rocket Physics
-MAIN_ENGINE_POWER = 200.0
+# Strong engine to catch the fall (TWR > 2.0)
+MAIN_ENGINE_POWER = 300.0 
 SIDE_ENGINE_POWER = 20.0
 INITIAL_FUEL = 100.0
-FUEL_CONSUMPTION_RATE = 5.0  
+FUEL_CONSUMPTION_RATE = 5.0
 
-# --- Rocket Dimensions (In Meters) ---
-# Decoupling these from SCALE allows the zoom to work properly.
-ROCKET_H_WIDTH = 0.5  # Half-width (Total width = 1.0 meter)
-ROCKET_HEIGHT = 2.5   # Total Height
-NOSE_HEIGHT = 1.0     # Height of the nose cone
+# Rocket Dimensions (Meters)
+ROCKET_H_WIDTH = 1.0  # Wider body = more air resistance
+ROCKET_HEIGHT = 5.0   
+NOSE_HEIGHT = 2.0
 
 # Landing Zone
-PAD_WIDTH_METERS = 5  # The pad is 3.5 meters wide in the world
-PAD_HEIGHT_METERS = 0.4
-# We don't set pixels here anymore; we calculate them in the visualizer
+PAD_WIDTH_METERS = 10.0  # Bigger pad for high-speed landings
+PAD_HEIGHT_METERS = 1.0
 
-# Terrain / World
+# Terrain
 GRAVITY_X = 0.0
-GRAVITY_Y = -10.0  
-WIND_POWER_MAX =1.0 # <--- CHANGED: Set to 0.0 to disable wind for now
+GRAVITY_Y = -9.8  
+WIND_POWER_MAX = 1.0
 
 # Colors (R, G, B)
 LANDER_COLOR = (128, 102, 230)      # Purple
