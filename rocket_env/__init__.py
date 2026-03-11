3827ba5777a62478bbe0eff7157ad13ac1c9f1ff
MathewJobey <mathewjobey17@gmail.com>
2026-03-11 18:57:55 +0530

    cleaning the innit by removing old phases

--- rocket_env/__init__.py
+++ rocket_env/__init__.py
@@ -1,19 +1,4 @@
 from gymnasium.envs.registration import register
-
-# 1. Your original environment (Full game)
-register(
-    id='RocketLander-v0',
-    entry_point='rocket_env.rocket_lander:RocketLander',
-    max_episode_steps=1000, 
-)
-
-# 2. Your new Phase 1 training environment
-register(
-    id='Phase2Descent-v0',
-    entry_point='rocket_env.phase2_descent:Phase2Descent',
-    max_episode_steps=2000, 
-)
-
 # 3. Your new Phase 3 training environment
 register(
     id='Phase3Final-v0',
