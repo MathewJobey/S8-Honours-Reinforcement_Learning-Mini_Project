e361daad24771e851785cdaa551e081818c56f35
MathewJobey <mathewjobey17@gmail.com>
2026-03-23 21:43:56 +0530

    new register for phase3_app

--- rocket_env/__init__.py
+++ rocket_env/__init__.py
@@ -5,3 +5,9 @@ register(
     entry_point='rocket_env.phase3_final:Phase3Final',
     max_episode_steps=1100, 
 )
+# 4. Your Web Dashboard Environment
+register(
+    id='Phase3App-v0',
+    entry_point='rocket_env.phase3_app:Phase3App',
+    max_episode_steps=2000, 
+)
